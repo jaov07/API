ac8c73f4d339b201604bddf2a9e7238b072c4553
jaov07 <joaovictor70678@gmail.com>
2025-06-26 19:36:13 -0300

    integra rota post com banco de dados

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import express from 'express'
 import mysql from 'mysql2'
+
+
+
 const conexao = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -15,15 +18,17 @@ const users = []
 
 //postando usuário
 app.post('/users', (req,res)=>{
-    console.log(req);
-    try {
-        users.push(req.body)
-        res.status(201).json(req.body)
-    } catch (error) {
-        res.send("Algo deu errado")
-    }
+    const {nome, estado} = req.body
+    let sql = "insert into users (nome, estado) values (?, ?)"
+    conexao.query(sql, [nome, estado], (err, results)=>{
+        if(err){
+            return res.status(500).send("erro ao dar o post", err.message)
+        }
+        res.status(201).json(results)
+    })
 
-    res.send("Usuário Publicado Com sucesso")
+
+    
     
 })
 
@@ -49,7 +54,7 @@ app.get('/users/:id', (req,res) =>{
     let sql = "select * from users where id = ?"
     conexao.query(sql, [idUser], (err, results) =>{
         if(err){
-            res.status(500).send("erro ao buscar por ID: ", err.message)
+            return res.status(500).send("erro ao buscar por ID: ", err.message)
         }
         res.status(200).json(results)
     })
