ddc36b3f798cf67c42e6eebb1ea130f850aa697f
jaov07 <joaovictor70678@gmail.com>
2025-06-25 22:55:31 -0300

    Post user, Get user by ID and get all users

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 import express from 'express'
-
 const app = express()
+app.use(express.json())
+const users = []
 
+//postando usuário
+app.post('/users', (req,res)=>{
+    console.log(req);
+    try {
+        users.push(req.body)
+    } catch (error) {
+        res.send("Algo deu errado")
+    }
+
+    res.send("Usuário Publicado Com sucesso")
+    
+})
+//obtendo todos os usuários
 app.get('/users',(req, res) =>{
-    res.send("consegui acessar")
+    res.send(users)
+})
+
+//obtendo usuário por ID
+app.get('/users/:id', (req,res) =>{
+     let idUser = parseInt(req.params.id)
+    res.send(users[idUser - 1])
 })
 
 
